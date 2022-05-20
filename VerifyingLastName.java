9cc06896621246a03aac4d59fe6c7b9ce80a1ab2
Polumatisrilakshmi <polumatisrilakshmi@gmail.com>
2022-05-20 22:33:35 +0530

    Added: Verifying Last Name

---
+++ VerifyingLastName.java
@@ -0,0 +1,29 @@
+package COM.BRIDGELABZ;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class VerifyingLastName {
+    static Scanner scanner = new Scanner(System.in);
+
+        public void firstName() {
+            System.out.println("Enter the first name: ");
+            String name = scanner.next();
+            Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
+            Matcher match = pattern.matcher(name);
+
+            if (match.matches()) {
+                System.out.println("Name is valid");
+            } else {
+                System.out.println("Name is invalid, Try with another name.");
+            }
+        }
+
+        public static void main(String[] args) {
+
+            VerifyingFirstName check = new VerifyingFirstName();
+            check.firstName();
+        }
+    }
+
+
